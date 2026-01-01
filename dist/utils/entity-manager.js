3e5f89c43f8d20424530b4ca674b7162bf93c01f
tiff <tiff@klarmann.dev>
2026-01-01 13:26:36 +0100

    set timeout not necessary

--- dist/utils/entity-manager.js
+++ dist/utils/entity-manager.js
@@ -286,10 +286,10 @@ export class Card extends HTMLElement {
       
       const card = this.querySelector(selector);
       const fullname = `${name}${id}`;
-      setTimeout(() => {
+      // setTimeout(() => {
         card.appendChild(this.em._addCard(type, name, config, id, track));
         
-      }, 0);
+      // }, 0);
     } catch (e) {
       console.error("Error adding Card to selector", {selector:selector, id:id, msg:e})
     }
